5c80b7dbe64c457847a5d7c67b87922a4a304c2e
AllLook <alet5289@gmail.com>
2022-08-10 23:50:26 +0300

    Четная прогрессия исправление

--- DZ_C_1.4/Program.cs
+++ DZ_C_1.4/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-Console.WriteLine("Введите число больше 1");
+Console.WriteLine("Введите целое положительное число больше 1");
 int a = Convert.ToInt32(Console.ReadLine());
 int i = 2;
 while(i <= a)
