16b9b5d5e03240405f5b96dbcf4cec71d338366b
AllLook <alet5289@gmail.com>
2022-08-10 23:48:03 +0300

    Четная прогрессия

---
+++ DZ_C_1.4/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Введите число больше 1");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+while(i <= a)
+{
+  Console.Write($"{i} ");
+  i = i + 2; // Переназначает i даже где бы оно не было(вне цикла)?
+}
+
+
+
