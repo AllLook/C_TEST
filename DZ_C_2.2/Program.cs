953f3144a87c2225d55fe8cbf3b0ef928c3f8323
AllLook <alet5289@gmail.com>
2022-08-12 02:27:40 +0300

    Третье слева

---
+++ DZ_C_2.2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a >= 100)
+{string b = a.ToString();
+Console.Write($"{b[2]}");
+}
+else
+{
+    Console.WriteLine("Неверный ввод или ошибка");
+}
