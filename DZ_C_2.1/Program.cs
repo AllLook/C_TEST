7f246de2c8e3bb610152bb4dd40940524a76c332
AllLook <alet5289@gmail.com>
2022-08-12 02:12:05 +0300

    Второе из трехзначного

---
+++ DZ_C_2.1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое трехзначное число:");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a >= 100 &&  a < 1000)
+{int b = (a % 100) / 10;
+Console.Write($"{b}");
+}
+else
+{
+    Console.WriteLine("Неверный ввод или ошибка");
+}
