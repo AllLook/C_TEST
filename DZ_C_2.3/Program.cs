c18e6781289ffe56d3f531d6ce8defe9cc09be36
AllLook <alet5289@gmail.com>
2022-08-13 00:16:00 +0300

    Дни недели

---
+++ DZ_C_2.3/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine("Введите число по счету дня недели:");
+int a = Convert.ToInt32(Console.ReadLine());
+if( a == 6)
+{
+
+Console.Write("Это выходной!");
+}
+
+else if( a == 7)
+{
+  Console.Write("Это выходной!");
+    
+}
+
+
+else
+{
+
+Console.Write("Опять рабочие будни :(");
+}
