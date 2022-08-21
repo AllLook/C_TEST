b2535426d1449b0d19a21c94c69c8997602ac5ab
AllLook <alet5289@gmail.com>
2022-08-21 03:07:14 +0300

    Одно в степень другого

---
+++ DZ_C_4.1/Program.cs
@@ -0,0 +1,33 @@
+﻿          // Так проще, решение в одну строку цикл здесь не к чему по моему))
+
+//Console.WriteLine("Введите число:"); 
+//double A = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите степень для числа:"); 
+//double B = Convert.ToInt32(Console.ReadLine());
+//void Degree(double A,double B)
+//{
+//double Result =  Math.Pow(A, B); 
+//Console.Write($"Результат {Result}");
+//}
+//Degree(A,B);
+
+
+
+Console.WriteLine("Введите число:"); 
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень для числа:"); 
+int  B = Convert.ToInt32(Console.ReadLine());
+async void Degree(int A,int B)
+{
+  int Res = 1; // на 0 умножать нельзя всегда 0 будет
+  
+ for(int i = 1 ; i <= B; i ++ ) // начиная с 1 и до 5 вкл. 
+      {
+        Res *= A; // первый раз само число,прошлый результат умножается на исходное число и перезаписывается заданное кол во раз
+        
+      } 
+
+    Console.WriteLine($"Результат {Res}");
+}
+
+Degree(A,B);
