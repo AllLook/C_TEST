8b709e45d2c4c24be558824bd88ca940f9738f81
AllLook <alet5289@gmail.com>
2022-08-22 13:37:02 +0300

    Создание массива и вывод на экран

---
+++ DZ_C_4.3/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int [] CreateArray(int Lng) // функция создания массива
+{
+    int [] array = new int[Lng]; // создаем массив с кол вом элементов
+    for(int i = 0; i < Lng; i++) 
+    {
+        array[i] = new Random().Next(); // заполняем массив случайными элементами
+        
+    }
+    return array; 
+}
+
+void PrintArray( int [] array) // функция для вывода массива
+{
+    int Len = array.Length; // переменная с длинной массива
+    for( int i = 0; i < Len; i++)
+    {
+        Console.WriteLine($"{array[i]}" ); // по очереди выводим элементы массива
+    }
+}
+
+
+PrintArray(CreateArray(8));
