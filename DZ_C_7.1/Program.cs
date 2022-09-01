38aacb6010bf1b777cfd336302d4ea788c2dd73a
AllLook <alet5289@gmail.com>
2022-09-01 19:35:30 +0300

    Вещественный двумерный массив

---
+++ DZ_C_7.1/Program.cs
@@ -0,0 +1,29 @@
+﻿double[,] CreateMatrix(int rowCount, int columnCount, int min, int max)
+{
+    double[,] array = new double[rowCount, columnCount]; // создание двухмерного массива
+    for (int row = 0; row < rowCount; row++) // по строкам массива
+    {
+        for (int column = 0; column < columnCount; column++) // по столбцам
+        {
+            array[row, column] = new Random().NextDouble() * new Random().Next(min, max);
+            array[row, column] = Math.Round(array[row, column], 2);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(double[,] array)
+{
+    for (int row = 0; row < array.GetLength(0); row++)
+    {
+        for (int column = 0; column < array.GetLength(1); column++)
+        {
+            Console.Write($"{array[row, column]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] matrix = CreateMatrix(5, 3, 0, 99);
+PrintMatrix(matrix);
+
