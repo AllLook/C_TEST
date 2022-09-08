686b9df0aa1e4cac475e0250cf095302eab37cfb
AllLook <alet5289@gmail.com>
2022-09-08 16:39:29 +0300

    Сортировка по строкам

--- DZ_C_7.3.2/Program.cs
+++ DZ_C_7.3.2/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Количество строк ");
+int rowCount = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Количество столбцов");
+int columnCount = Convert.ToInt32(Console.ReadLine());
+int[,] CreateMatrix(int rowCount, int columnCount, int min, int max)
+{
+    int[,] array = new int[rowCount, columnCount]; // создание двухмерного массива
+    for (int row = 0; row < rowCount; row++) // по строкам массива
+    {
+        for (int column = 0; column < columnCount; column++) // по столбцам
+        {
+            array[row, column] = new Random().Next(min, max);
+
+        }
+    }
+    return array;
+}
+
+//int i = 0;
+
+//double[] arr = new double[columnCount]; 
+
+
+void PrintMatrix(int[,] array)
+{    
+    for (int row = 0; row < array.GetLength(0); row++)
+    {
+         for (int column = 0; column < array.GetLength(1); column++)
+        {
+            Console.Write($"{array[row, column]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Average(int[,] array)
+{   
+    int colums = array.GetLength(1);// переменная с кол вом столбцов
+    int rows = array.GetLength(0); // переменная с кол вом строк 
+    double [] arr = new double[colums]; // новый массив размером с кол во  столбцов
+
+    for (int column = 0; column < colums; column++)// цикл по столбцам 
+    {   
+        for (int row = 0; row < rows; row++) //цикл по строкам 
+        {
+
+            arr[column] += array[row, column]; // индекс столбца 
+            
+        }
+
+        Console.WriteLine($"{Math.Round(arr[column]/ rows , 2)}");
+    }
+    //Console.WriteLine($"{arr[i] / rowCount}");
+}
+int[,] matrix = CreateMatrix(rowCount, columnCount, 0, 10);
+PrintMatrix(matrix);
+Average(matrix);
---
+++ DZ_C_8.1/Program.cs
@@ -0,0 +1,64 @@
+﻿/* Console.WriteLine("Количество строк ");
+int rowCount = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Количество столбцов");
+int columnCount = Convert.ToInt32(Console.ReadLine()); */
+int[,] CreateMatrix(int rowCount, int columnCount, int min, int max)
+{
+    int[,] array = new int[rowCount, columnCount]; // создание двухмерного массива
+    for (int row = 0; row < rowCount; row++) // по строкам массива
+    {
+        for (int column = 0; column < columnCount; column++) // по столбцам
+        {
+            array[row, column] = new Random().Next(min, max);
+
+        }
+    }
+    return array;
+}
+
+
+
+void PrintMatrix(int[,] array)
+{    
+    for (int row = 0; row < array.GetLength(0); row++)
+    {
+         for (int column = 0; column < array.GetLength(1); column++)
+        {
+            Console.Write($"{array[row, column]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Sorting(int[,] array)
+{   
+   //int rows = array.GetLength(0); // переменная с кол вом строк
+    //int colums = array.GetLength(1);// переменная с кол вом столбцов
+    //int [,] arr = new int[rows,colums]; // новый массив размером с кол во  столбцов
+    for (int row=0; row < array.GetLength(0); row++) //цикл по строкам 
+    {   
+        for (int column = 0; column < array.GetLength(1); column++)// цикл по столбцам 
+        {
+            for(int col = column + 1; col < array.GetLength(1); col ++) // по столбцам след элемент
+            {
+                
+                if(array[row,column] < array[row,col]) // сравнение текущего и следующего
+                {
+                   
+                   int temp = array[row,column] ;
+                   array[row,column] = array[row,col];
+                   array[row,col] = temp;
+                
+                }
+            }
+        }
+        
+        }   
+        Console.WriteLine();
+        return array;
+    }
+
+int[,] matrix = CreateMatrix(3, 3, 0, 10);
+PrintMatrix(matrix);
+Sorting(matrix);
+PrintMatrix(matrix);
