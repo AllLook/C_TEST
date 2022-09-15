4ee3717b6d63bd5f05d7c2453a7ccb2ef6ff9dd9
AllLook <alet5289@gmail.com>
2022-09-15 22:05:39 +0300

    Сортировка по убыванию

--- DZ_C_8.1/Program.cs
+++ DZ_C_8.1/Program.cs
@@ -1,8 +1,8 @@
-﻿
-/* Console.WriteLine("Количество строк ");
+﻿Console.WriteLine("Количество строк ");
 int rowCount = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Количество столбцов");
-int columnCount = Convert.ToInt32(Console.ReadLine()); */
+int columnCount = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
 int[,] CreateMatrix(int rowCount, int columnCount, int min, int max)
 {
     int[,] array = new int[rowCount, columnCount]; // создание двухмерного массива
@@ -11,16 +11,17 @@ int[,] CreateMatrix(int rowCount, int columnCount, int min, int max)
         for (int column = 0; column < columnCount; column++) // по столбцам
         {
             array[row, column] = new Random().Next(min, max);
-
         }
     }
     return array;
 }
+//int i = 0;
+//double[] arr = new double[columnCount]; 
 void PrintMatrix(int[,] array)
-{
+{    
     for (int row = 0; row < array.GetLength(0); row++)
     {
-        for (int column = 0; column < array.GetLength(1); column++)
+         for (int column = 0; column < array.GetLength(1); column++)
         {
             Console.Write($"{array[row, column]}\t");
         }
@@ -28,60 +29,35 @@ void PrintMatrix(int[,] array)
     }
 }
 
-int[] Sum(int[,] array)
-{  //int res = 0;
-    int rows = array.GetLength(0); // переменная с кол вом строк
-    int colums = array.GetLength(1);// переменная с кол вом столбцов
-    int[] arr = new int[rows]; // новый массив размером с кол во  строк
-    for (int row = 0; row < rows; row++) //цикл по строкам 
-    {
-        for (int column = 0; column < colums; column++)// цикл по столбцам 
+
+int[,] Sorting(int[,] array)
+{   
+   //int rows = array.GetLength(0); // переменная с кол вом строк
+    //int colums = array.GetLength(1);// переменная с кол вом столбцов
+    //int [,] arr = new int[rows,colums]; // новый массив размером с кол во  столбцов
+    for (int row=0; row < array.GetLength(0); row++) //цикл по строкам 
+    {   
+        for (int column = 0; column < array.GetLength(1); column++)// цикл по столбцам 
         {
+            for(int col = column + 1; col < array.GetLength(1); col ++) // по столбцам след элемент
+            {
+                
+                if(array[row,column] < array[row,col]) // сравнение текущего и следующего
+                {
 
+                   int temp = array[row,column] ; // текущий индекс во временную переменную
+                   array[row,column] = array[row,col]; // текущий  который добавляется в массив равен знач из условия
+                   array[row,col] = temp;// следующий стал текущим
 
-            arr[row] += array[row, column]; // значения накапливаются в текущий индекс нового массива 
-
+                }
+            }
         }
-        /*  Console.WriteLine();
-         Console.Write(arr[row]); */
+        
+        }   
+        Console.WriteLine();
+        return array;
     }
-    return arr;
-
-}
-void PrintAr(int[] arr)
-{
-    //Console.Write(String.Join(" " , arr));
-    for (int i = 0; i < arr.Length; i++)
-    {
-        Console.Write($" {arr[i]}");
-    }
-}
-
-void Sort(int[] arr)
-{
-    int number = 0;
-    int min = arr[0];
-    for (int i = 0; i < arr.Length; i++)
-    {
-        //int min = arr[i];
-        if (arr[i] < min)
-        {
-            arr[i] = min;
-            number += 1;
-        }
-
-
-        /*   Console.WriteLine();
-          Console.Write($"{min}");  */
-    }
-    Console.WriteLine();
-    Console.Write($"Строка с наименьшим значением {number}"); // вывод для пользователя
-}
-
-int[,] matrix = CreateMatrix(3, 3, 0, 10);
+int[,] matrix = CreateMatrix(rowCount, columnCount, 0, 10);
 PrintMatrix(matrix);
-Sum(matrix);
-int[] maximum = Sum(matrix);
-PrintAr(maximum);
-Sort(maximum);
-//PrintMatrix(matrix);
+Sorting(matrix);
+PrintMatrix(matrix);
